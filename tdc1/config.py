5d7de48a05225e69bad36286a8d552612bf9340f
mtewes@astro.uni-bonn.de <mtewes@astro.uni-bonn.de@ada317e2-8117-4487-a3a6-e33dcca33bbc>
2014-06-10 15:41:35 +0000

    But all filepaths etc here

---
+++ tdc1/config.py
@@ -0,0 +1,15 @@
+
+
+# Where the "tdc1" directory is:
+#datadir = "/users/mtewes/fohlen/TDC/tdc1_v2_7Apr2014"
+datadir = "/users/mtewes/fohlen/TDC/tdc1_v2_7Apr2014"
+
+
+xtrastatsdir = "/users/mtewes/fohlen/TDC/tdc1_v2_7Apr2014/xtrastats"
+
+
+
+
+
+d3cslogpath = "../d3cs_logs/2014-06-07.txt"
+
