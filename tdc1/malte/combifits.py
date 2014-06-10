fd9480e268559be4e77a638cdc33e194b3f3e7e1
mtewes@astro.uni-bonn.de <mtewes@astro.uni-bonn.de@ada317e2-8117-4487-a3a6-e33dcca33bbc>
2014-06-10 15:41:02 +0000

    seem to work

--- tdc1/malte/combifits.py
+++ tdc1/malte/combifits.py
@@ -1,6 +1,110 @@
+"""
+We combine info from:
+	- D3CS results
+	- PyCS results
+	- "xtrastats"
+	- stats resulting from the time delays (e.g., overlap)
+into a single big pkl & FITS table
+
+
+"""
+
+execfile("config.py")
+
 import pycs
 import os
 import numpy as np
+import pyfits
+
+
+db = {} # we'll use a dictionnary for building the big unique db, easier to index, and convert it to a list later.
+
+# We initialize the db with empty dicts.
+for rung in range(5):
+	#for pair in pycs.tdc.util.listtdc1v2pairs():
+	for pair in range(1, 1037):
+		pairid = "%s_%i_%i" % ("tdc1", rung, pair)
+		db[pairid] = {}
+		
+
+
+
+##############   D3CS    ##############
+
+
+d3csests = pycs.tdc.est.importfromd3cs(d3cslogpath)
+
+groupedd3csests = pycs.tdc.est.group(d3csests)
+for group in groupedd3csests:
+	db[group[0].id]["d3cs_n"] = len(group) # Number of D3CS estimates
+
+
+combid3csests = pycs.tdc.est.multicombine(d3csests, method='d3cscombi1')
+
+for est in combid3csests:
+		
+	db[est.id]["d3cs_td"] = est.td
+	db[est.id]["d3cs_tderr"] = est.tderr
+	db[est.id]["d3cs_ms"] = est.ms
+	db[est.id]["d3cs_timetaken"] = est.timetaken
+	
+	
+		
+print "Done with D3CS..."
+##############   xtrastats    ##############
+
+
+for rung in range(5):
+	for pair in pycs.tdc.util.listtdc1v2pairs():
+		pairid = "%s_%i_%i" % ("tdc1", rung, pair)
+	
+		relfilepath = pycs.tdc.util.tdcfilepath(set="tdc1", rung=rung, pair=pair, skipset=False)
+		pklpath = os.path.join(xtrastatsdir, relfilepath+".stats.pkl")
+		if os.path.exists(pklpath):
+			
+			data = pycs.gen.util.readpickle(pklpath)
+			db[pairid].update(data) # We add this dict content to the existing dict.
+
+
+
+print "Done with xtrastats..."
+##############   PyCS    ##############
+
+
+
+
+##############   overlap etc    ##############
+
+
+
+
+##############   FITS export    ##############
+
+
+
+# Now we convert this dict to a list. Important as we will rely on the ordering !
+
+db = db.values() # A plain list of dicts, as usual.
+
+# Get a flat list of all the available keys, to be used as columns.
+allkeys = []
+for e in db:
+	allkeys.extend(e.keys())
+allkeys = sorted(list(set(allkeys)))
+
+print "Columns: "
+print allkeys
+
+cols = [pyfits.Column(name=key, format="D", array=np.array([item.get(key) for item in db])) for key in allkeys]
+	
+coldefs = pyfits.ColDefs(cols)
+tbhdu = pyfits.new_table(coldefs)
+
+fitsfilepath = "test.fits"
+
+tbhdu.writeto(fitsfilepath, clobber=True)
+print "Wrote %s" % (fitsfilepath)
+
 
 """
 d3csests = pycs.tdc.est.importfromd3cs("/Users/mtewes/Desktop/TDC/d3cs_logs/2014-06-06_danka_removed.txt")
@@ -14,32 +118,7 @@ douests = pycs.tdc.est.select(d3csests,idlist=dousel)
 combidouests = pycs.tdc.est.multicombine(douests, method='d3cscombi1')
 
 pycs.gen.util.writepickle(combidouests, "combidouests.pkl")
-"""
-
 
 combidouests = pycs.gen.util.readpickle("combidouests.pkl")
+"""
 
-
-def export(estimates, filepath):
-	"""
-	Saves the estimates into a FITS table for interactive inspection with tools like topcat
-	"""
-	
-	try:
-		import pyfits
-	except:
-		print "pyfits could not be imported, no problem if you don't need it."
-	
-	fields = ["rung", "pair", "td", "tderr", "ms", "confidence", "timetaken"]
-	
-	cols = [pyfits.Column(name=a, format="D", array=np.array([getattr(e, a) for e in estimates])) for a in fields]
-	
-	coldefs = pyfits.ColDefs(cols)
-	tbhdu = pyfits.new_table(coldefs)
-	tbhdu.writeto(filepath, clobber=True)
-	print "Wrote %s" % (filepath)
-	
-
-
-
-export(combidouests, "test.fits")
--- tdc1/malte/xtrastats.py
+++ tdc1/malte/xtrastats.py
@@ -1,16 +1,8 @@
 """
 Save some time-delay-indpendant stats about the light curves into pkl files, one per pair.
-
-
 """
 
-datadir = "/users/mtewes/fohlen/TDC/tdc1_v2_7Apr2014"
-outdir = "/users/mtewes/fohlen/TDC/tdc1_v2_7Apr2014/xtrastats"
-
-#datadir = "/Users/mtewes/Desktop/TDC/tdc1_v2_7Apr2014"
-#outdir = "/Users/mtewes/Desktop/out"
-
-
+execfile("config.py")
 
 import pycs
 import os
@@ -59,27 +51,31 @@ def variostats(lcs):
 
 
 	
-for rung in [0, 1, 2, 3, 4]:
-	for pair in pycs.tdc.util.listtdc1v2pairs():
+#for rung in [0, 1, 2, 3, 4]:
+#	for pair in pycs.tdc.util.listtdc1v2pairs():
 
 #for rung in [0]:
-#	for pair in [1]:
+#	for pair in pycs.tdc.util.listtdc1v2pairs():
 
 		relfilepath = pycs.tdc.util.tdcfilepath(set="tdc1", rung=rung, pair=pair, skipset=False)
+		outpath = os.path.join(xtrastatsdir, relfilepath+".stats.pkl")
+		if os.path.exists(outpath):
+			print "Skipping %s, already done." % (outpath)
+			continue
+		
+		
 		lcs = pycs.tdc.util.read(os.path.join(datadir, relfilepath), mag="asinh", verbose=True, shortlabel=False)
 
-		#res = {}
 		res = variostats(lcs)
 		res["rung"] = rung
 		res["pair"] = pair
 		
-		print res
+		#print res
 		
-		outpath = os.path.join(outdir, relfilepath+".stats.pkl")		
+			
 		if not os.path.exists(os.path.dirname(outpath)):
 			os.makedirs(os.path.dirname(outpath))
 		pycs.gen.util.writepickle(res, outpath)		
-		
 		
 		
 		
