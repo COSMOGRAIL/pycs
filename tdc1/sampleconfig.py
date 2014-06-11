0db1650f842bebbb6e7417dea27959ba4c505116
mtewes@astro.uni-bonn.de <mtewes@astro.uni-bonn.de@ada317e2-8117-4487-a3a6-e33dcca33bbc>
2014-06-11 13:52:17 +0000

    adding pycsresdir

--- tdc1/sampleconfig.py
+++ tdc1/sampleconfig.py
@@ -6,6 +6,9 @@ datadir = "/users/mtewes/fohlen/TDC/tdc1_v2_7Apr2014"
 # Directory in which should I store the xtrastats:
 xtrastatsdir = "/users/mtewes/fohlen/TDC/tdc1_v2_7Apr2014/xtrastats"
 
+# Directory for PyCS "output" (whatever format we will choose...)
+pycsresdir = "results_tdc1"
+
 # Which D3CS log should I use:
 d3cslogpath = "d3cs_logs/2014-06-07.txt"
 
